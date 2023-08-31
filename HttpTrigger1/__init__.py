9d0618500995a4426807e9652fc10a3206b16b85
tylerkline <tylerkline@gmail.com>
2023-08-31 10:24:03 -0400

    second commit

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,5 +1,4 @@
 import logging
-
 import azure.functions as func
 from cryptography.fernet import Fernet
 import zlib
@@ -8,22 +7,15 @@ import json
 from datetime import datetime
 import openai
 from OpenAIAuth import Auth0
-
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from http.cookies import SimpleCookie
 
 def login_to_openai():
     auth = Auth0(email_address=client.get_secret("OPENAIUSERNAME").value, password=client.get_secret("OPENAIPW").value)
     access_token = auth.get_access_token()
     return access_token
 
-def encode_to_utf8(current_log, data):
-    if isinstance(data, (dict, list)):
-        return current_log+"\n"+json.dumps(data).encode('utf-8')
-    elif isinstance(data, str):
-        return current_log+"\n"+data.encode('utf-8')
-    elif isinstance(data, (int, float)):
-        return current_log+"\n"+str(data).encode('utf-8')
-    else:
-        raise ValueError("Unsupported data type. Please provide a dictionary, list, or string.")
 
 HEADERS_RESPONSE = {
     "statusCode": 201,
@@ -65,12 +57,24 @@ OPENAI_FORMAT = {
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    
-    cipher_suite = Fernet(client.get_secret("ENCRYPTIONKEY").value)
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url="https://llms2155671127.vault.azure.net/", credential=credential)
+    key = client.get_secret("ENCRYPTIONKEY").value
 
-    encrypted_input = req.cookies.get("payload")
-    #req_body = req.get_json()
-    decrypted_input_str = cipher_suite.decrypt(encrypted_input.encode()).decode()
+    cipher_suite = Fernet(key)
+    cookie_header = req.headers.get('Cookie')
+    cookies = []
+    if cookie_header:
+        if "PID" in cookie_header:
+            cookie_pairs = cookie_header.split('; ')
+            for pair in cookie_pairs:
+                key, value = pair.split('=', 1)
+                cookie = cipher_suite.decrypt(value.encode()).decode()
+                cookies.append(cookie)
+    headers = {
+        "Set-Cookie": SimpleCookie('PID='+json.dumps(cookies)),
+    }
+    response = func.HttpResponse(body="", status_code=200, headers=headers)
 
-    return func.HttpResponse(body="", cookies={"result": encrypted_output})
+    return response
 
