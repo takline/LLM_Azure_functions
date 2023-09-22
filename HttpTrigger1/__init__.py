92a99ab86c9ceb3b8aaaf4156cf1707f8cf91867
tylerkline <tylerkline@gmail.com>
2023-09-22 12:35:42 -0400

    fixing logging

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -23,8 +23,8 @@ def create_httpresponse_from_dict(data: dict) -> func.HttpResponse:
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logger.info(json.dumps(req.headers))
-    logger.info(req.get_body().decode("utf-8"))
+    logging.info(json.dumps(req.headers))
+    logging.info(req.get_body().decode("utf-8"))
     cookie_header = req.headers.get("Cookie")
     if cookie_header is None:
         cookie_header = req.get_body().decode("utf-8")
